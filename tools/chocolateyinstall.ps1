9ff410e270ebb024dd6cdb43065b4c8156be8d5c
Steve Taylor <staylorx@gmail.com>
2020-10-12 09:19:10 -0600

    Update for 4.2.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 $ErrorActionPreference = 'Stop'
 
 $PackageName = 'camunda-modeler'
-$url32       = 'https://downloads.camunda.cloud/release/camunda-modeler/3.7.3/camunda-modeler-3.7.3-win-ia32.zip'
-$url64       = 'https://downloads.camunda.cloud/release/camunda-modeler/3.7.3/camunda-modeler-3.7.3-win-x64.zip'
-$checksum32  = '00B488ACD63FE87F7AC365BC518FC796FDB942C5B63B6F90787D5B517EAC0C1D'
-$checksum64  = '7C7CB08CEC86A39827F6FB1C5CFD0EC5A1D5CAF21EC7CD65F455F5F1B7E29BBB'
+$url32       = 'https://downloads.camunda.cloud/release/camunda-modeler/4.2.0/camunda-modeler-4.2.0-win-ia32.zip'
+$url64       = 'https://downloads.camunda.cloud/release/camunda-modeler/4.2.0/camunda-modeler-4.2.0-win-x64.zip'
+$checksum32  = '91A02C3D0BDDC27D9B9255970B0EB45C2D8703078596C0020C4D6A5D34614152'
+$checksum64  = '9A15424819D408D1C58FD8245FEDE7675CC0B1A88CBC1C10911B1E6AB434724B'
 
 $packageArgs = @{
   packageName    = $packageName
