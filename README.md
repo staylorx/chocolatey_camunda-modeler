57f67670a709f39f19f9e0d8bbaad373918b82fc
Steve Taylor <staylorx@gmail.com>
2023-09-11 11:07:48 -0600

    Update 5.15.0

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Camunda Modeler
 
+The binaries for this are now coming from GitHub release artifacts instead of the Camunda download page.
+
 ## Licenses and Ownership
 
 All product names, logos, brands, trademarks and registered trademarks are property of their respective owners. The use of the Camunda modeler icon and logo are to support the hosting of the the package and do not imply ownership or endorsement. I'm using the icon in this repo to satisfy the CDN requirement for chocolatey; no ownership on my part is implied or expressed.
--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 $ErrorActionPreference = 'Stop'
 
 $packageName = 'camunda-modeler'
-$url32 = 'https://downloads.camunda.cloud/release/camunda-modeler/5.8.0/camunda-modeler-5.8.0-win-ia32.zip'
-$url64 = 'https://downloads.camunda.cloud/release/camunda-modeler/5.8.0/camunda-modeler-5.8.0-win-x64.zip'
-$checksum32 = 'ce42d5296eeeca316be6e50cc67af10053bc9bc8d8e246cbbb5c8dc0ab3b0db3'
-$checksum64 = '4d50b55ab7d022d31925de3049a633cffb37f1726e1faf7852e889610aeac975'
+$url32 = 'https://github.com/camunda/camunda-modeler/releases/download/v5.15.0/camunda-modeler-5.15.0-win-ia32.zip'
+$url64 = 'https://github.com/camunda/camunda-modeler/releases/download/v5.15.0/camunda-modeler-5.15.0-win-x64.zip'
+$checksum32 = '74562459b298142481cff492af0c4c2ad24dd1cc6a0d5a7a0e2b183e115de9ea'
+$checksum64 = 'b8c1acaa1f5ff4b542d2b79cf1875cd37f2061aa90671bdc7a859e9b03e71879'
 
 $packageArgs = @{
   packageName    = $packageName
